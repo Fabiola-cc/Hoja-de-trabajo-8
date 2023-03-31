f3799dd0da4a989d0f417cfe649b677e26a713f2
Fabiola Contreras <fcontrerascol@gmail.com>
2023-03-31 11:42:49 -0600

    Funcionamiento de métodos en controlador
    Implementación de la interfaz y uso de heap dentro de controlador1

--- src/Controlador1.java
+++ src/Controlador1.java
@@ -1,18 +1,38 @@
 import java.util.ArrayList;
-import Heap_de_Moises.HeapUsingIterativeBinaryTree;
 
-public class Controlador1 implements ICreacion_heap{
+
+import Heap_de_Moises.HeapUsingIterativeBinaryTree;
+import Heap_de_Moises.ComparadorNumeros;
+
+public class Controlador1 implements ICreacion_heap<proceso>{
+    HeapUsingIterativeBinaryTree<Integer, proceso> heap1;
+    Controlador1(){
+        heap1 = new HeapUsingIterativeBinaryTree<>(new ComparadorNumeros<>());
+    }
 
     @Override
-    public void agregar_proceso_a_HEAP(ArrayList a) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'agregar_proceso_a_HEAP'");
+    public void agregar_proceso_a_HEAP(ArrayList<proceso> procesos_registrados) {
+        for (proceso pr: procesos_registrados) {
+            int priority = pr.getPriority();
+            String[] data = {pr.getNombre_proceso(), pr.getUsuario()};
+
+            heap1.Insert(priority, pr);
+        }
     }
 
     @Override
     public void imprimir_HEAp() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'imprimir_HEAp'");
+        for (int i = 0; i < heap1.count(); i++) {
+            proceso temp = heap1.get();
+            System.out.println("temp.toString()");
+            System.out.println("");
+        }
+
+        while(!heap1.isEmpty()){
+            proceso temp = heap1.remove();
+            System.out.println("");//toString 2
+        }
+
     }
 
 
