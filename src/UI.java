75d221f4c6be0c548a1110cc2259d34bd2e9db75
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-03-30 18:11:48 -0600

    Implementación de UI y txt de procesos

--- src/UI.java
+++ src/UI.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UI {
+    static readFile read = new readFile();
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> total = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("\nBienvenidos a la nueva distribución de Linux");
+        System.out.println("\n CHICHICASTELINUX");
+        System.out.println("En este momento se leera el archivo de código...");
+        total = read._readfile("src\\procesos.txt");
+        System.out.println("Se han guardado tus datos de procesos");
+        boolean salir = false;
+        System.out.println("\t El órden de las prioridades el el siguiente");
+        //Métódo de prioridades 
+
+}
 }
